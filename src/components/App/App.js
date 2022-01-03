0536de556516b4d0ec88bd8b4cea5a0dd5d208f7
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-01-02 23:56:35 -0300

    Issues with background image transition.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,32 @@ import { globalState } from "../../globalState";
 import Header from '../Header/Header';
 import Page from "../Page/Page";
 
-export const AppContext = createContext(null)
+export const AppContext = createContext(null);
 
 function App() {
     const [state, setState] = useState(globalState);
+
     return(
-        <div className="container-fluid">
-            <AppContext.Provider value={{state, setState}}>
+        <AppContext.Provider value={{state, setState}}>
+            <div className={renderBackground()}>
                 <Header />
                 <Page />
-            </AppContext.Provider>
-        </div>
+            </div>
+        </AppContext.Provider>
     )
+
+    // METHODS
+
+    // renderBackground() changes the background image based on temperature.
+
+    function renderBackground() {
+        const max_temp = 15;
+        if(!state.weatherInfo) {
+            return 'container-fluid container-dimensions app-cold';
+        } else if(state.weatherInfo.main.temp > max_temp) {
+            return 'container-fluid container-dimensions app-cold app-warm';
+        } else return 'container-fluid container-dimensions app-cold';
+    }
 }
 
 export default App;
