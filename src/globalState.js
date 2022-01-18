a1d1d874c09b81cd9030aae00570b0c85ca014f7
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-01-18 20:49:08 -0300

    Just testing!

--- src/globalState.js
+++ src/globalState.js
@@ -1,11 +1,10 @@
 export const globalState = {
     isLoading: false,
     access_token: '9fe118ad0aa74437ba7190406220201',
-    googleMaps_key: 'AIzaSyDHXbcwPrcYHsLmWNOAO-zR-sUOq6rO7WM',
     base_url: 'https://api.weatherapi.com/v1',
     apiMethod: '/forecast.json',
     location: null, 
-    forecastDays: 3,
+    forecastDays: 7,
     weatherInfo: false,
     requestErrorWasFound: false
 }
